33bdcefab11472a95ba939f21ba65735bce1f3dd
FlareV2Pringle <64933809+FlareV2Pringle@users.noreply.github.com>
2020-05-07 15:46:48 -0400

    Update VisualModule.cs

--- Flare Sharp/ClientBase/Modules/VisualModule.cs
+++ Flare Sharp/ClientBase/Modules/VisualModule.cs
@@ -7,69 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Flare_Sharp.ClientBase.Modules
-{
-    public abstract class VisualModule : Module
-    {
-        public int tFontSize = 72;
-        public int fontSize = 32;
-        public Font titleFont;
-        public Font textFont;
-        public Font textFontSmall;
-        public float scale = 1;
-        public SolidBrush primary
-        {
-            get
-            {
-                return OverlayHost.ui.primary;
-            }
-        }
-        public SolidBrush secondary
-        {
-            get
-            {
-                return OverlayHost.ui.secondary;
-            }
-        }
-        public SolidBrush tertiary
-        {
-            get
-            {
-                return OverlayHost.ui.tertiary;
-            }
-        }
-        public SolidBrush quaternary
-        {
-            get
-            {
-                return OverlayHost.ui.quaternary;
-            }
-        }
-        public SolidBrush quinary
-        {
-            get
-            {
-                return OverlayHost.ui.quinary;
-            }
-        }
-        public SolidBrush rainbow
-        {
-            get
-            {
-                return OverlayHost.ui.rainbow;
-            }
-        }
+namespace Flare_Sharp.ClientBase.Modules {
+ public abstract class VisualModule: Module {
+  public int tFontSize = 72;
+  public int fontSize = 32;
+  public Font titleFont;
+  public Font textFont;
+  public Font textFontSmall;
+  public float scale = 1;
+  public SolidBrush primary {
+   get {
+    return OverlayHost.ui.primary;
+   }
+  }
+  public SolidBrush secondary {
+   get {
+    return OverlayHost.ui.secondary;
+   }
+  }
+  public SolidBrush tertiary {
+   get {
+    return OverlayHost.ui.tertiary;
+   }
+  }
+  public SolidBrush quaternary {
+   get {
+    return OverlayHost.ui.quaternary;
+   }
+  }
+  public SolidBrush quinary {
+   get {
+    return OverlayHost.ui.quinary;
+   }
+  }
+  public SolidBrush rainbow {
+   get {
+    return OverlayHost.ui.rainbow;
+   }
+  }
 
-        public VisualModule(string name, Category category, int keybind, bool enabled) : base(name, category, keybind, enabled)
-        {
-            titleFont = new Font(new FontFamily("Arial"), tFontSize * scale, FontStyle.Regular, GraphicsUnit.Pixel);
-            textFont = new Font(new FontFamily("Arial"), fontSize * scale, FontStyle.Regular, GraphicsUnit.Pixel);
-            textFontSmall = new Font(new FontFamily("Arial"), fontSize * scale / 2, FontStyle.Regular, GraphicsUnit.Pixel);
-        }
+  public VisualModule(string name, Category category, int keybind, bool enabled): base(name, category, keybind, enabled) {
+   titleFont = new Font(new FontFamily("Arial"), tFontSize * scale, FontStyle.Regular, GraphicsUnit.Pixel);
+   textFont = new Font(new FontFamily("Arial"), fontSize * scale, FontStyle.Regular, GraphicsUnit.Pixel);
+   textFontSmall = new Font(new FontFamily("Arial"), fontSize * scale / 2, FontStyle.Regular, GraphicsUnit.Pixel);
+  }
 
-        public virtual void onDraw(Graphics graphics)
-        {
+  public virtual void onDraw(Graphics graphics) {
 
-        }
-    }
+  }
+ }
 }
